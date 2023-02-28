e78e3f1d4ecf6eeb8ddebba6b1894afffe886d59
Omar Sayed <omar.sayed001@eng-st.cu.edu.eg>
2023-02-28 19:33:29 +0200

    .

--- TESTBENCH/testbench.v
+++ TESTBENCH/testbench.v
@@ -3,11 +3,19 @@ reg MOSI_tb,SS_n_tb,rst_n_tb,clk_tb;
 wire MISO_tb;
 integer counter;
 initial begin
-    clk = 0;
+    clk_tb = 0;
     forever begin
-        #T/2 clk = ~clk;
+        #15 clk_tb = ~clk_tb;
     end
 end
 
     MEMORY_BLOCK_WRAPPER DUT ( MOSI_tb,MISO_tb,SS_n_tb,clk_tb,rst_n_tb);
+initial begin
+    rst_n_tb=0;
+    MOSI_tb=0;
+    SS_n_tb=0;
+    
+end
+
+
 endmodule
